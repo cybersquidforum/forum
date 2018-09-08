3d578112f80c7b697afafa1912357a43351b4ccb
Martin <arti99@mail.bg>
2018-09-09 02:57:27 +0300

    Migration
    Fix migration path.

--- src/ForumServiceProvider.php
+++ src/ForumServiceProvider.php
@@ -14,7 +14,7 @@ class ForumServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/cybersquid.php');
 
-        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'cybersquid');
 
@@ -22,8 +22,8 @@ class ForumServiceProvider extends ServiceProvider
 
         // Publish migrations
         $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('/migrations')
-        ], 'migrations');
+            __DIR__ . '/../migrations/' => database_path('/migrations')
+        ], 'cybersquid-migrations');
 
         // Publish routes
         $this->publishes([

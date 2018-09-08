19420f0dba0de48dee33c97417a6b5d75aaa8127
Martin <arti99@mail.bg>
2018-09-08 06:17:58 +0300

    Models
    Added Models.
    Fixed namespace.

---
+++ src/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cybersquid\Forum\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model 
+{
+
+    protected $table = 'categories';
+    public $timestamps = true;
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    public function forums()
+    {
+        return $this->hasMany('Forums');
+    }
+
+}
